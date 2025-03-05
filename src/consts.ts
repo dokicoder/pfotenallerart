f400d64575e42968b1911f954815b0fbeec3233e
Kay Illner <kay.illner@cistec.com>
2025-03-05 17:56:03 +0100

    chore: adjustments

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Pfoten aller Art - Liebevolle Haustierbetreuung in Berlin';
-export const SITE_DESCRIPTION = 'Professionelle und liebevolle Betreuung für Haustiere aller Art in Berlin und Umgebung. Hunde, Katzen, Kleintiere und mehr.';
+export const SITE_TITLE = 'Pfoten aller Art - Liebevolle Haustierbetreuung in Winterthur Hegi';
+export const SITE_DESCRIPTION = 'Professionelle und liebevolle Betreuung für Haustiere aller Art in Winterthur und Umgebung. Hunde, Katzen, Kleintiere und mehr.';
+
+export const PHONE_NUMBER = '+41 79 123 45 67';
+export const EMAIL_ADDRESS = 'photenallerart@gmail.com';
+export const ADDRESS = '8404 Winterthur';
+export const AREA = 'Winterthur Hegi';
